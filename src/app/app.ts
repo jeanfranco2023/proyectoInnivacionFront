4dbf8b5d38b6ede69d89ac289f51b8273d9869c6
greis <greischurampi@gmail.com>
2026-04-15 16:53:36 -0500

    feat: implementacion de buscador, microfono, subida de archivos en el chat y el sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ChatComponent } from './chatbot/chat/chat';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  standalone: true,
+  imports: [ChatComponent]
 })
-export class App {
-  protected readonly title = signal('proyectoInnovacion');
-}
+export class App {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// Si el error persiste con appConfig, prueba pasar un objeto vacío o solo los providers necesarios
+bootstrapApplication(App, {
+  providers: [
+    provideAnimations()
+  ]
+}).catch((err) => console.error(err));
